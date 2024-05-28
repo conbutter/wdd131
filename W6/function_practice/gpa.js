d1cf69f8f50e4ed4e7860758e864fe7cd271b290
conbutter <but20031@byui.edu>
2024-05-28 17:25:39 -0600

    Added new GPA site test.

---
+++ W6/function_practice/gpa.js
@@ -0,0 +1,51 @@
+function getGrades(inputSelector) {
+    const grades = document.querySelector(inputSelector).value;
+    const gradesArray = grades.split(",");
+    const cleanGrades = gradesArray.map((grade) => grade.trim().toUpperCase());
+    console.log(cleanGrades);
+    return cleanGrades;
+}
+  
+function lookupGrade(grade) {
+    // converts the letter grade to it's GPA point value and returns it
+    let points = 0;
+    if (grade === "A") {
+      points = 4;
+    } else if (grade === "B") {
+      points = 3;
+    } else if (grade === "C") {
+      points = 2;
+    } else if (grade === "D") {
+      points = 1;
+    }
+    return points;
+}
+  
+function calculateGpa(grades) {
+    // gets a list of grades passed in
+    // convert the letter grades to gpa points
+    const gradePoints = grades.map((grade) => lookupGrade(grade));
+    // calculates the GPA
+    const gpa =
+      gradePoints.reduce((total, num) => total + num) / gradePoints.length;
+    // return the GPA
+    return gpa.toFixed(2);
+}
+  
+function outputGpa(gpa, selector) {
+    // takes a gpa value and displays it in the HTML in the element identified by the selector passed in
+    const outputElement = document.querySelector(selector);
+    outputElement.innerText = gpa;
+}
+  
+function clickHandler() {
+    // when the button in our html is clicked
+    // get the grades entered into the input
+    const grades = getGrades("#grades");
+    // calculate the gpa from the grades entered
+    const gpa = calculateGpa(grades);
+    // display the gpa
+    outputGpa(gpa, "#output");
+}
+  
+document.querySelector("#submitButton").addEventListener("click", clickHandler);
