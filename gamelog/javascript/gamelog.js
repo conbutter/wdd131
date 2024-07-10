dbe02c6898d4989e4f454c18bce591575a31493b
conbutter <but20031@byui.edu>
2024-07-10 10:37:57 -0600

    Added new content.

--- gamelog/javascript/gamelog.js
+++ gamelog/javascript/gamelog.js
@@ -5,6 +5,7 @@ function switchVisualMode() {
     const body = document.body;
     const header = document.querySelector("header");
     const footer = document.querySelector("footer");
+    const search_img = document.querySelector(".search-button");
     const darkModeButton = document.querySelector(".dark-mode");
 
     if (darkMode == 0) {
@@ -13,6 +14,7 @@ function switchVisualMode() {
         body.style.color = "#ffffff";
         header.style.backgroundColor = "#392B58";
         footer.style.backgroundColor = "#1f1f1f";
+        search_img.src = "images/search_white.svg";
         darkModeButton.textContent = "Light Mode";
     } else {
         darkMode = 0;
@@ -20,6 +22,7 @@ function switchVisualMode() {
         body.style.color = "#000000";
         header.style.backgroundColor = "#392B58";
         footer.style.backgroundColor = "rgb(31, 31, 31)";
+        search_img.src = "images/search.svg";
         darkModeButton.textContent = "Dark Mode";
     }
 
@@ -30,6 +33,7 @@ function applyVisualMode() {
     const body = document.body;
     const header = document.querySelector("header");
     const footer = document.querySelector("footer");
+    const search_img = document.querySelector(".search-button");
     const darkModeButton = document.querySelector(".dark-mode");
 
     if (darkMode == 1) {
@@ -37,12 +41,14 @@ function applyVisualMode() {
         body.style.color = "#ffffff";
         header.style.backgroundColor = "#392B58";
         footer.style.backgroundColor = "#1f1f1f";
+        search_img.src = "images/search_white.svg";
         darkModeButton.textContent = "Light Mode";
     } else {
         body.style.backgroundColor = "#fcfbfb";
         body.style.color = "#000000";
         header.style.backgroundColor = "#392B58";
         footer.style.backgroundColor = "rgb(31, 31, 31)";
+        search_img.src = "images/search.svg";
         darkModeButton.textContent = "Dark Mode";
     }
 }
