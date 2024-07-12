1cfa904316ce87da68f0d395135d1760a1838ef1
conbutter <but20031@byui.edu>
2024-07-11 20:06:42 -0600

    Rolled back content to fix bugs.

--- gamelog/javascript/gamelog.js
+++ gamelog/javascript/gamelog.js
@@ -272,20 +272,23 @@ document.addEventListener('click', function(event) {
 function toggleFavoriteStatus(favoriteButton) {
     const gameName = favoriteButton.dataset.gameName;
     const favoriteIndex = games.findIndex(game => game.name === gameName);
+
     if (favoriteIndex !== -1) {
+        // Toggle favorite status
         games[favoriteIndex].favorite = games[favoriteIndex].favorite === 1 ? 0 : 1;
+
+        // Change button color based on favorite status
         if (games[favoriteIndex].favorite === 1) {
             favoriteButton.style.color = darkMode === 1 ? 'red' : 'red';
         } else {
             favoriteButton.style.color = darkMode === 1 ? 'white' : 'black';
         }
+
         init();
     }
-    // Testing code for console log, keep disabled at launch
-    // console.log(gameName, favoriteIndex, games[favoriteIndex].favorite)
 }
 
 // TO-DO:
+// + Fix favorite button
 // + Add title add / removal functionality
 // + Add update display functionality when content is added / removed
-// + Style add / remove forms
